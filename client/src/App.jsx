c51137a1245423cb40b68e8c1f01e139ef351efa
Harkirat Singh <singharkirath1511@gmail.com>
2025-11-02 19:13:26 +0530

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
+import Home from './pages/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <Router>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+    </Routes>
+  </Router>
+  )
+}
+
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='bg-amber-300'>Home</div>
+  )
+}
+
+export default Home
